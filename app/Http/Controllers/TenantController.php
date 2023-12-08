057d0cda7ea8729325a8c8e74160632bc26625bd
山田優太 <>
2023-12-08 14:20:17 +0900

    vital.blade

--- app/Http/Controllers/TenantController.php
+++ app/Http/Controllers/TenantController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Tenant;
+use App\Models\Vital;
+use Laravel\Ui\Presets\Vue;
 
 class TenantController extends Controller
 {
@@ -20,7 +22,7 @@ class TenantController extends Controller
     }
 
     /**
-     * 商品登録
+     * 入居者登録
      */
     public function add(Request $request)
     {
@@ -44,11 +46,55 @@ class TenantController extends Controller
         return view('tenant.add');
     }
 
-    //削除
+    /**
+     * 入居者削除
+     */
     public function delete(Request $request){
         $tenant = Tenant::find($request->id);
         $tenant->delete();
 
         return redirect('/tenants');
     }
+
+     /**
+     * バイタル一覧表示
+     */
+    public function vital(Request $request)
+    {
+        // バイタル一覧取得
+        $vitals = Vital::where('tenant_id', '=', $request->id)->get();
+        $tenants = Tenant::where('id', '=', $request->id)->first();
+        
+
+        return view('tenant.vital', compact('vitals','tenants'));
+    }
+
+    /**
+     * バイタル登録
+     */
+    public function vital_add(Request $request, $id)
+    {
+        // POSTリクエストのとき
+        if ($request->isMethod('post')) {
+            // バリデーション
+            $this->validate($request, [
+                
+            ]);
+            // バイタル登録
+            Vital::create([
+                'user_id' => Auth::user()->id,
+                'tenant_id' => $request->id,
+                'kt' => $request->kt,
+                'sbp' => $request->sbp,
+                'dbp' => $request->dbp,
+                'spo2' => $request->spo2,
+            ]);
+
+
+            return redirect('tenants/vital/'.$request->id);
+        }
+
+        return view('tenant.vital_add', ['id'=>$id]);
+    }
+
 }
--- app/Models/Vital.php
+++ app/Models/Vital.php
@@ -8,4 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class Vital extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'tenant_id',
+        'kt',
+        'sbp',
+        'dbp',
+        'spo2',
+    ];
+
+     /**
+        * Tenant(入居者)の保持する全vital
+        */
+        public function tenant()
+        {
+            return $this->hasMany(Tenant::class);
+        }
+
+
+      /**
+        * バイタルのテーブルにあるユーザーID
+        */
+        public function user()
+        {
+            return $this->belongsTo(User::class);
+        }
+
 }
